000865ec58c29e5c9a1529c6852789dad803358b
ShibanshL <shibansh.w@gmail.com>
2022-03-01 17:30:19 +0530

    Added a new file

---
+++ index_1.js
@@ -0,0 +1,73 @@
+// The multiple reducers //
+// Actions //
+
+const redux = require('redux')
+const createStore = redux.createStore
+
+const buy_cake = 'BUY_CAKE'
+
+const buy_icecream = 'BUY_ICECREAM'
+
+
+
+function actionCreator(){
+    return {
+        type:buy_cake,
+        info:'First redux action'
+    }
+}
+
+function actionCreator2(){
+    return {
+        type:buy_icecream,
+        info:'Second redux action'
+    }
+}
+
+//(prevState, action) = newState
+
+const initialCakeState = {
+    numOfCakes:20,
+}
+
+const initialIceCreamState = {
+    numOfIceCream:20,
+}
+
+const reducer = (state=initialIceCreamState, action) => {
+    switch(action.type){
+        case buy_icecream : return{
+            ... state,
+            numOfIceCream: state.numOfIceCream - 2,
+        }
+        default: return state
+    }
+}
+
+const reducer2 = (state=initialCakeState, action) => {
+    switch(action.type){
+        case buy_cake : return{
+            ... state,
+            numOfCakes: state.numOfCakes - 1,
+        }
+        default: return state
+    }
+}
+
+const store = createStore(reducer)
+console.log('Initial state ',store.getState())
+const unsubscribe = store.subscribe(() =>console.log('Updated State ',store.getState()))
+
+
+store.dispatch(actionCreator())
+store.dispatch(actionCreator())
+store.dispatch(actionCreator())
+
+// unsubscribe()
+
+store.dispatch(actionCreator2())
+store.dispatch(actionCreator2())
+store.dispatch(actionCreator2())
+
+
+unsubscribe()
