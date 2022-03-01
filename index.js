96d930822548eac348916b39d3b7a1246b6e5034
ShibanshL <shibansh.w@gmail.com>
2022-03-01 17:11:09 +0530

    trying my own thing

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const createStore = redux.createStore
 
 const buy_cake = 'BUY_CAKE'
 
+
+const buy_icecream = 'BUY_ICECREAM'
+
 function actionCreator(){
     return {
         type:buy_cake,
@@ -13,17 +16,28 @@ function actionCreator(){
     }
 }
 
+function actionCreator2(){
+    return {
+        type:buy_icecream,
+        info:'Second redux action'
+    }
+}
+
 //(prevState, action) = newState
 
 const initialState = {
-    numOfCakes:20
+    numOfCakes:20,
+
+    numOfIceCream:20
 }
 
 const reducer = (state=initialState, action) => {
     switch(action.type){
         case buy_cake : return{
             ... state,
-            numOfCakes: state.numOfCakes-1
+            numOfCakes: state.numOfCakes - 1,
+
+            numOfIceCream : state.numOfIceCream + 1
         }
 
         default: return state
@@ -33,9 +47,17 @@ const reducer = (state=initialState, action) => {
 const store = createStore(reducer)
 console.log('Initial state ',store.getState())
 const unsubscribe = store.subscribe(() =>console.log('Updated State ',store.getState()))
+
+
 store.dispatch(actionCreator())
 store.dispatch(actionCreator())
 store.dispatch(actionCreator())
+
+unsubscribe()
+
+store.dispatch(actionCreator2())
+store.dispatch(actionCreator2())
+store.dispatch(actionCreator2())
 
 
 unsubscribe()
