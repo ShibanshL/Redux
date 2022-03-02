f140193839a827497360eba97039cb2922a87a7c
ShibanshL <shibansh.w@gmail.com>
2022-03-02 16:11:38 +0530

    Yep I am changing

--- index_Async_Actions.js
+++ index_Async_Actions.js
@@ -7,3 +7,24 @@ const initialState = {
 const fetch_user_request = 'fetch_user_request'
 const fetch_user_success = 'fetch_user_success'
 const fetch_user_failure = 'fetch_user_failure'
+
+
+const fetchUserRequest = () =>{
+    return{
+        type:fetch_user_request
+    }
+}
+
+const fetchUserSuccess = user =>{
+    return{
+        type:fetch_user_success,
+        payload:user
+    }
+}
+
+const fetchUserError = error =>{
+    return{
+        type:fetch_user_failure,
+        payload:error
+    }
+}
